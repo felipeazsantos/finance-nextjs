a4eea269fac136e174f794e8076d1e72582b14ac
felipeazsantos <felipeasantos10@hotmail.com>
2025-08-29 17:29:39 -0300

    UserButton to logout from clerk

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,6 +1,10 @@
-import { Button } from '@/components/ui/button';
-import Image from 'next/image';
+import { UserButton } from '@clerk/nextjs';
 
 export default function Home() {
-  return <p>This is an authenticated route</p>;
+  return (
+    <div>
+      <UserButton afterSignOutUrl='/' />
+      <p>This is an authenticated route</p>
+    </div>
+  );
 }
