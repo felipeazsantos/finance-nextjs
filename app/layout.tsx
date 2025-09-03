5b3a26d09c233d1eb64aa9a4e6ec649df4884e67
felipeazsantos <felipeasantos10@hotmail.com>
2025-09-03 18:53:19 -0300

    building accounts sheet component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,14 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import { Inter } from 'next/font/google';
 import './globals.css';
 import { ClerkProvider } from '@clerk/nextjs';
 import { QueryProvider } from '@/providers/query-provider';
+import { SheetProvider } from '@/providers/sheet-provider';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
+const inter = Inter({
+  variable: '--font-inter',
   subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
+  display: 'swap',
 });
 
 export const metadata: Metadata = {
@@ -27,10 +24,11 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang='en'>
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
-          <QueryProvider>{children}</QueryProvider>
+        <body className={`${inter.variable} antialiased`}>
+          <QueryProvider>
+            <SheetProvider />
+            {children}
+          </QueryProvider>
         </body>
       </html>
     </ClerkProvider>
---
+++ features/accounts/components/new-account-sheet.tsx
@@ -0,0 +1,24 @@
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+} from '@/components/ui/sheet';
+
+const NewAccountSheet = () => {
+  return (
+    <Sheet open>
+      <SheetContent className='space-y-4'>
+        <SheetHeader>
+          <SheetTitle>New Account</SheetTitle>
+          <SheetDescription>
+            Create a new account to track your transactions.
+          </SheetDescription>
+        </SheetHeader>
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default NewAccountSheet;
